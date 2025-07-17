17272a780905b554b1fb47e52667a51be0af7bbe
Charles7c <charles7c@126.com>
2025-07-17 20:16:47 +0800

    feat(core): SpringUtils 工具类新增 getBean(Class<T> clazz, boolean ignoreNoSuchBeanEx) 方法

--- continew-starter-core/src/main/java/top/continew/starter/core/util/SpringUtils.java
+++ continew-starter-core/src/main/java/top/continew/starter/core/util/SpringUtils.java
@@ -17,6 +17,7 @@
 package top.continew.starter.core.util;
 
 import cn.hutool.extra.spring.SpringUtil;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 /**
  * Spring 工具类
@@ -39,5 +40,26 @@ public class SpringUtils {
      */
     public static <T> T getProxy(T target) {
         return (T)SpringUtil.getBean(target.getClass());
+    }
+
+    /**
+     * 通过 class 获取 Bean
+     *
+     * @param <T>                Bean类型
+     * @param clazz              Bean类
+     * @param ignoreNoSuchBeanEx 是否忽略 {@link NoSuchBeanDefinitionException}
+     * @return Bean对象
+     * @see SpringUtil#getBean(Class)
+     * @since 2.13.1
+     */
+    public static <T> T getBean(Class<T> clazz, boolean ignoreNoSuchBeanEx) {
+        try {
+            return SpringUtil.getBean(clazz);
+        } catch (NoSuchBeanDefinitionException e) {
+            if (ignoreNoSuchBeanEx) {
+                return null;
+            }
+            throw e;
+        }
     }
 }
